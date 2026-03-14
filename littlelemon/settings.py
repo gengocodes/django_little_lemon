0e81fa5b4a85e3b273d9355402c3e6017977e8a0
gengocodes <paulcorsino28@gmail.com>
2026-03-14 13:19:30 +0800

    fix secretkey

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", 'secret-key-hehe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
